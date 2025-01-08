55f8526b90bf240034f5e8255c03b6c2c5cdb63d
ArtemPopa <you@example.com>
2025-01-08 12:51:17 +0300

    Начло уровня

---
+++ Level.py
@@ -0,0 +1,23 @@
+class Level:
+    def startScriptedObject():
+        pass
+
+
+    def triggerNpc():
+        pass
+
+
+    def switchPlayerControllability():
+        pass
+
+
+    def switchTimeMachine():
+        pass
+
+
+    def setBackgroundMusic():
+        pass
+
+
+    def playSound():
+        pass
