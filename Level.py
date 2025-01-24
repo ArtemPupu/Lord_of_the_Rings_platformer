78e8033eb042c6d9f88fab847fd37e1098e4fe5d
ArtemPopa <you@example.com>
2025-01-24 20:19:46 +0300

    Начало уровня

--- Level.py
+++ Level.py
@@ -1,25 +1,35 @@
-class Level:
-    def startScriptedObject():
-        pass
+from pygame import *
+import os
+
+PLATFORM_WIDTH = 32
+PLATFORM_HEIGHT = 32
+PLATFORM_COLOR = "#000000"
+ICON_DIR = os.path.dirname(__file__)  # Полный путь к каталогу с файлами
+
+ANIMATION_BLOCKTELEPORT = [
+    ('%s/blocks/portal2.png' % ICON_DIR),
+    ('%s/blocks/portal1.png' % ICON_DIR)]
+
+ANIMATION_PRINCESS = [
+    ('%s/blocks/princess_l.png' % ICON_DIR),
+    ('%s/blocks/princess_r.png' % ICON_DIR)]
 
 
-    def triggerNpc():
-        pass
+class Platform(sprite.Sprite):
+    def __init__(self, x, y):
+        sprite.Sprite.__init__(self)
+        self.image = Surface((PLATFORM_WIDTH, PLATFORM_HEIGHT))
+        self.image.fill(Color(PLATFORM_COLOR))
+        self.image = image.load("%s/blocks/platform.png" % ICON_DIR)
+        self.image.set_colorkey(Color(PLATFORM_COLOR))
+        self.rect = Rect(x, y, PLATFORM_WIDTH, PLATFORM_HEIGHT)
 
 
-    def switchPlayerControllability():
-        pass
-
-
-    def switchTimeMachine():
-        pass
-
-
-    def setBackgroundMusic():
-        pass
-
-
-    def playSound():
-        pass
-
-
+class Platform1(sprite.Sprite):
+    def __init__(self, x, y):
+        sprite.Sprite.__init__(self)
+        self.image = Surface((PLATFORM_WIDTH, PLATFORM_HEIGHT))
+        self.image.fill(Color(PLATFORM_COLOR))
+        self.image = image.load("%s/blocks/grunt.png" % ICON_DIR)
+        self.image.set_colorkey(Color(PLATFORM_COLOR))
+        self.rect = Rect(x, y, PLATFORM_WIDTH, PLATFORM_HEIGHT)
